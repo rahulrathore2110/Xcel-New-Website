f6b3fe6010019cd5d0d8be5e8f815a6cc382916e
rahulrathore2110 <rahulg27727@gmial.com>
2023-12-19 12:24:50 +0530

    Addition

---
+++ script/nav.js
@@ -0,0 +1,26 @@
+// navbar script
+
+let navbar = document.querySelector(".nav-items");
+
+let close = document.getElementById("close");
+let open = document.getElementById("navbars");
+
+open.addEventListener("click", () => {
+  shownav(navbar);
+});
+
+close.addEventListener("click", () => {
+  hidenav(navbar);
+});
+
+function shownav(navbar) {
+  navbar.style.visibility = "visible";
+  open.style.visibility = "hidden";
+  close.style.visibility = "visible";
+}
+
+function hidenav(navbar) {
+  navbar.style.visibility = "hidden";
+  open.style.visibility = "visible";
+  close.style.visibility = "hidden";
+}
